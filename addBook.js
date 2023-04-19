e6a4c8327f482de4f9d3dbb7d875ab942f0ba27d
Raeid Usmanali <raeidus15@gmail.com>
2023-04-19 13:25:20 -0400

    bug fixes

---
+++ addBook.js
@@ -0,0 +1,8 @@
+/*function testFunction() {
+alert('This is a test to see if HTML and JS can converse')
+}*/
+
+function addNewBook(addBookForm) {
+    var titBookLS = addBookForm.titBookL.value
+    alert(titBookLS)
+}
